65597e7772b7c9af9dc4c2b4ef29f9426551aff3
root <root@ASUS-ROG-G713RM-LL055.localdomain>
2023-11-09 00:55:10 +0300

    12345

--- random_source.h
+++ random_source.h
@@ -1,11 +1,13 @@
 typedef struct RandomSource RandomSource;
 
-typedef struct RandomSourceOperations {
-    RandomSource * (*destroy)(RandomSource *src);
+typedef struct RandomSourceOperations
+{
+    RandomSource *(*destroy)(RandomSource *src);
     double (*next)(RandomSource *src);
 } RandomSourceOperations;
 
-typedef struct RandomSource {
+typedef struct RandomSource
+{
     RandomSourceOperations *opt;
     unsigned long long base;
     int fd;
